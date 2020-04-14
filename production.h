219e0d965cefb6df12f0fb780a95c0c62f4e74aa
Darrel Maddy <ndm12@newcastle.ac.uk>
2020-04-14 22:22:00 +0000

    Deleted production.h

--- production.h
+++
@@ -1,8 +0,0 @@
-
-// This file is used to indicate if the code is being run for debug or production.
-// If running for debug comment out the next line, if running for production leave the next line in
-// #define PRODUCTION_RUN
-
- #define PRODUCTION_RUN true
-
-//
